8b79490976d58ce2e79c7ba0d636224a53ef2927
Zachary John D. Lavengco <74513869+ZachLavengco@users.noreply.github.com>
2022-02-28 17:14:41 -0600

    Add files via upload

---
+++ desktop/build/resources/main/maps/Items.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Items" tilewidth="32" tileheight="32" tilecount="45" columns="9">
+ <image source="Tiles-Items-pack.png" width="288" height="160"/>
+ <tile id="1" type="Treasure"/>
+ <tile id="2" type="Treasure"/>
+ <tile id="10" type="SpeedPot"/>
+ <tile id="22" type="Treasure"/>
+</tileset>
